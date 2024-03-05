ecf6a0119b48cb28987643f10819221460e9d1c5
sakethram113 <rampodicheti1234@gmail.com>
2024-03-05 14:20:43 +0530

    created a server using expressjs

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.status(200).send("Welcome aboard");
+});
+app.get("/register", (req, res)=>{
+    res.status(200).send("Registration Page is now live");
+});
+
+const PORT = 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
+})
